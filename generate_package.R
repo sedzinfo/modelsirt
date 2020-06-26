0ef7d44481b937bb0053b77ecb1d04d1809214ea
dimitrios <dimitrios@psycholate.com>
2020-06-26 11:44:07 +0300

    add cmd check

--- generate_package.R
+++ generate_package.R
@@ -1,9 +1,9 @@
 ##########################################################################################
 # DIRECTORIES
 ##########################################################################################
-# R CMD check snp
-# R CMD Rd2pdf snp
-# R CMD build snp --resave-data
+# R CMD check modelsirt
+# R CMD Rd2pdf modelsirt
+# R CMD build modelsirt --resave-data
 library(devtools)
 library(roxygen2)
 setwd("/mnt/WD500/public_rstatistics/")
